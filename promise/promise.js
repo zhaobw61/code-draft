b3abc19a7c4a150d669028f5b602c9b0088003be
zhaobw <1964471757@qq.com>
2020-07-18 19:40:37 +0800

    添加promise的链式调用

--- promise/promise.js
+++ promise/promise.js
@@ -1,6 +1,32 @@
 const PENDING = 'PENDING';
 const REJECTED = 'REJECTED';
 const FULFILLED = 'FULFILLED';
+const resolvePromise = (promise2, x, resolve, reject) {
+    // 处理返回值x的类型 来决定调用resolve还是reject
+    if(promise2 === x) {
+        return reject(new TypeError(`chaining cycle detected for promise #<Promise>`));
+    }
+    // 判断x是不是普通值
+    if((typeof x === 'object' && x !== null) || typeof x === 'function') {
+        // 可能是promise 通过判断有没有then方法
+        try{
+            let then = x.then;
+            if(typeof then === 'function'){
+                then.call(x, y=>{
+                    resolve(y);
+                },r=>{
+                    resolve(x);
+                });
+            }else{
+                resolve(x);
+            }
+        }catch(e){
+            reject(e); // 取then报错了
+        }
+    }else{
+        resolve(x)
+    }
+}
 class Promise {
     constructor(executor) {
         this.value = undefined;
@@ -8,7 +34,7 @@ class Promise {
         this.status = PENDING;
         this.onResolvedCallbacks = [];
         this.onRejectedCallbacks = [];
-        let reslove = value => {
+        let resolve = value => {
             if(this.status === PENDING){
                 this.value = value;
                 this.status = FULFILLED;
@@ -24,26 +50,58 @@ class Promise {
         };
         // 这里可能发生异常
         try {
-            executor(reslove, reject);
+            executor(resolve, reject);
         } catch (e) {
             reject(e);
         }
     }
 
     then(onFulfilled, onRejected) {
-        if(this.status == FULFILLED) {
-            onFulfilled(this.value);
-        }
-        if(this.status == REJECTED) {
-            onRejected(this.reason);
-        }
-        if(this.status == PENDING) {
-            this.onResolvedCallbacks.push(() => {
-                onFulfilled(this.value);
-            });
-            this.onRejectedCallbacks.push(()=> {
-                onRejected(this.value);
-            });
-        }
+        let promise2 = new Promise((resolve, reject)=> {
+            if(this.status == FULFILLED) {
+                setTimeout(() => {
+                    try {
+                        let x = onFulfilled(this.value);
+                        resolvePromise(promise2, x, resolve, reject);
+                    }catch(e) {
+                        reject(e);
+                    }
+                });
+            }
+            if(this.status == REJECTED) {
+                setTimeout(() => {
+                    try {
+                        let x = onRejected(this.reason);
+                        resolvePromise(promise2, x, resolve, reject);
+                    }catch(e) {
+                        reject(e);
+                    }
+                });
+            }
+            if(this.status == PENDING) {
+                this.onResolvedCallbacks.push(() => {
+                    setTimeout(() => {
+                        try {
+                            let x = onFulfilled(this.value);
+                            resolvePromise(promise2, x, resolve, reject);
+                        } catch (e) {
+                            reject(e);
+                        }
+                    });
+                });
+                this.onRejectedCallbacks.push(()=> {
+                    setTimeout(() => {
+                        try {
+                            let x = onRejected(this.value);
+                            resolvePromise(promise2, x, resolve, reject);
+                        } catch (e) {
+                            reject(e);
+                        }
+                    });
+                });
+            }
+        });
+
+        return promise2;
     }
 }
